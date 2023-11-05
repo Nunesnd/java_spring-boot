1b153895184d6f0a1e7f127f1b11ff6cd7457f71
Diego Nunes <nunesdiegond@gmail.com>
2023-11-05 20:01:15 -0300

    first commit

---
+++ src/main/java/com/cursogratis/appConsole/AppConsoleApplication.java
@@ -0,0 +1,17 @@
+package com.cursogratis.appConsole;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppConsoleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("################");
+		System.out.println("olá, mundo!");
+		System.out.println("################");
+
+		//SpringApplication.run(AppConsoleApplication.class, args);
+	}
+
+}
